25f11213d6596d0e7d61ed7c98a422179512741c
Kits <luck3801@gmail.com>
2024-04-23 16:18:57 -0300

    rect impl

--- idj_engine/src/arith/rect/mod.rs
+++ idj_engine/src/arith/rect/mod.rs
@@ -1,9 +1,41 @@
 #[cfg(test)]
 mod tests;
 
+use crate::arith::vec3::Vec3;
+
 struct Rect {
     x: f32,
     y: f32,
+    z: f32,
     w: f32,
     h: f32
+}
+
+impl Rect {
+    pub fn new(x: f32, y: f32, z: f32, w: f32, h:f32) -> Self {
+        Rect {
+            x,
+            y,
+            z,
+            w,
+            h
+        }
+    }
+
+    pub fn area(&self) -> f32{
+        return self.w * self.h;
+    }
+
+    pub fn center(&self) -> Vec3 {
+        return Vec3 {x: self.x + (self.w*0.5f32), y: self.y + (self.h*0.5f32), z: self.z}
+    }
+
+    pub fn set_center(&mut self, x: f32, y: f32) {
+        self.x = x - (self.w*0.5f32);
+        self.y = y - (self.h*0.5f32);
+    }
+
+    pub fn get_pos(&self) -> Vec3 {
+        return Vec3 {x: self.x, y: self.y, z: self.z}
+    }
 }
--- idj_engine/src/arith/rect/tests.rs
+++ idj_engine/src/arith/rect/tests.rs
@@ -3,32 +3,33 @@ use crate::arith::vec3::Vec3;
 
 #[test]
 fn test_creation() {
-    let new = Rect::new(1.0,2.0,3.0,4.0);
+    let new = Rect::new(1.0,2.0,3.0,4.0, 5.0);
     assert_eq!(new.x, 1.0);
     assert_eq!(new.y, 2.0);
-    assert_eq!(new.w, 3.0);
-    assert_eq!(new.h, 4.0);
+    assert_eq!(new.z, 3.0);
+    assert_eq!(new.w, 4.0);
+    assert_eq!(new.h, 5.0);
 }
 
 #[test]
 fn test_area() {
-    let new = Rect::new(1.0,2.0,3.0,4.0);
-    assert_eq!(new.area(),12.0);
+    let new = Rect::new(1.0,2.0,3.0,4.0, 5.0);
+    assert_eq!(new.area(),20.0);
 }
 
 #[test]
 fn test_center() {
-    let mut new = Rect::new(1.0,2.0,3.0,4.0);
+    let mut new = Rect::new(1.0,2.0,3.0,4.0, 5.0);
     let c = new.center();
-    assert_eq!(c , Vec3::new(2.5,4.0, 0.0));
+    assert_eq!(c , Vec3::new(3.0, 4.5, new.z));
 
-    new.set_center(0,0);
+    new.set_center(10.0,10.0);
     let c = new.center();
-    assert_eq!(c, Vec3::new(1.5,2.0,0.0));
+    assert_eq!(c, Vec3::new(10.0, 10.0, new.z));
 }
 
 #[test]
 fn test_get_pos() {
-    let new = Rect::new(1.0,2.0,3.0,4.0);
-    assert_eq!(new.get_pos(), Vec3::new(1.0,2.0,0.0))
+    let new = Rect::new(1.0,2.0,3.0,4.0, 5.0);
+    assert_eq!(new.get_pos(), Vec3::new(1.0,2.0,3.0))
 }
